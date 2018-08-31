3dd155dca5dea9ed5da7eea5e15014e3985f11dc
Axel Wilhelm Berle <axelberle@yahoo.de>
2018-08-31 09:29:39 -0300

    fixed wrong test resource path

--- src/test/java/support/TestRunner.java
+++ src/test/java/support/TestRunner.java
@@ -7,6 +7,6 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = false, dryRun = false, monochrome = false, plugin = { "html:target/site/cucumber-pretty",
-		"json:target/cucumber.json" }, tags = { "@MVP1-5075" }, features = "src/test/resources/", glue = "stepdefs")
+		"json:target/cucumber.json" }, tags = { "not @ignored" }, features = "src/test/resources/", glue = "stepdefs")
 public class TestRunner {
 }
